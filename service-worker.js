39fb1503c28f5d7349986d85cebc8726f843123f
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-03-04 17:10:04 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,25 @@
+// service-worker.js
+const CACHE_NAME = 'lens-prescription-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json'
+];
+
+// Install event
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('app-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/script.js',
-                '/icons/icon-192x192.png'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
 });
 
+// Fetch event
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
