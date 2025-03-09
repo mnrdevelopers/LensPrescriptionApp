f131beecd53ef4ee47abaee6fcbe19e09ae408ec
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-03-09 07:25:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,30 +53,6 @@ window.addEventListener("beforeunload", (event) => {
     }
 });
 
-
-document.addEventListener("DOMContentLoaded", () => {
-    const darkModeToggle = document.getElementById("darkModeToggle");
-
-    // Check if Dark Mode was enabled before
-    if (localStorage.getItem("darkMode") === "enabled") {
-        document.body.classList.add("dark-mode");
-        darkModeToggle.checked = true;
-    }
-
-    // Toggle Dark Mode on Switch Click
-    darkModeToggle.addEventListener("change", () => {
-        if (darkModeToggle.checked) {
-            document.body.classList.add("dark-mode");
-            localStorage.setItem("darkMode", "enabled");
-        } else {
-            document.body.classList.remove("dark-mode");
-            localStorage.setItem("darkMode", "disabled");
-        }
-    });
-});
-
-
-
 // Auto-fill the current date
 document.getElementById("currentDate").textContent = new Date().toLocaleDateString();
 
