86a7f22222c998c8995f25bb919b97e30bd743b2
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-03-17 14:51:10 +0530

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const footerHTML = `
-        <footer style="text-align: center; margin-top: 15px; font-size: 12px; color: white;">
+        <footer style="text-align: center; margin-top: 220px; font-size: 12px; color: white;">
             App developed by <strong>MANITEJA (MNR DEVELOPERS)</strong>
         </footer>
     `;
