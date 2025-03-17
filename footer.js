534a10f92a997d0ce2f688144706346acdd0025d
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-03-17 14:15:17 +0530

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const footerHTML = `
         <div style="text-align: center; margin-top: 15px; font-size: 12px; color: white;">
-            App developed by <strong>Maniteja</strong>
+            App developed by <strong>Maniteja (MNR DEVELOPERS)</strong>
         </div>
     `;
     document.body.insertAdjacentHTML("beforeend", footerHTML);
