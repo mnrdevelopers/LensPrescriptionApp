a67ad677c5c0a611803fe92c7c543199578a4b7e
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-03-17 14:54:22 +0530

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const footerHTML = `
-        <footer style="text-align: center; margin-top: 220px; font-size: 12px; color: white;">
-            App developed by <strong>MANITEJA (MNR DEVELOPERS)</strong>
+        <footer style="
+            text-align: center; 
+            margin-top: 220px; 
+            font-size: 12px;
+            font-weight: bolder;
+            color: white; 
+            background-color: #333; 
+            padding: 15px; 
+            position: relative;
+            bottom: 0;
+            width: 100%;">
+            @2025 Lens-Prescription-App developed by <strong>MANITEJA (MNR DEVELOPERS)</strong>
         </footer>
     `;
     document.body.insertAdjacentHTML("beforeend", footerHTML);
